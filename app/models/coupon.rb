5687eedd2dd9afadd5467a2ec3a3d86fd767417e
Jan <jannilsonn@gmail.com>
2021-04-05 17:49:21 -0300

    WIP promotion API

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,7 +3,13 @@ class Coupon < ApplicationRecord
 
     enum status: { enabled: 0, disabled: 1}
 
+    delegate :discount_rate, to: :promotion
+
     def self.search(query)
       find_by(code: query)
+    end
+
+    def as_json(options = {})
+      super({ methods: :discount_rate }.merge(options))
     end
 end
--- test/integration/promotion/api/show_test.rb
+++ test/integration/promotion/api/show_test.rb
@@ -17,5 +17,34 @@ class IntegrationPromotionApiShowTest < ActionDispatch::IntegrationTest
     assert_response :success
     body = JSON.parse(response.body, symbolize_names: true)
     assert_equal coupon.code, body[:code]
+    assert_equal promotion.discount_rate.to_s, body[:discount_rate]
+  end
+
+  test 'show coupon not found' do
+    get "/api/v1/coupons/0"
+
+    assert_response :not_found
+  end
+
+  test 'show invalid without header' do
+    assert_raises ActionController::RoutingError do
+      get "/api/v1/coupons/0"
+    end
+  end
+
+  test 'show coupon disabled' do
+    user = User.create!(name: 'Johnny Cage', email: 'johnny.cage@iugu.com.br', password: 'password')
+    promotion = Promotion.create!(name: 'Cyber Monday',
+                                  coupon_quantity: 100,
+                                  description: 'Promoção de Cyber Monday',
+                                  code: 'CYBER15',
+                                  discount_rate: 15,
+                                  expiration_date: '22/12/2033',
+                                  user: user)
+      coupon = Coupon.create!(code: 'NATAL10-0001', promotion: promotion)
+
+      get "/api/v1/coupons/0"
+
+      assert_response :not_found
   end
 end
